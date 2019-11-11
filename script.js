f9f3fd2b2e861f002b01880e1cada1e59336293f
Philip Ng <phil__n@hotmail.com>
2019-11-11 16:47:04 -0500

    created index.html and script.js files; created all elements required in the html and created variables to pull the inputs in the html elements

---
+++ script.js
@@ -0,0 +1,12 @@
+var symbols = "!@#$%^&*()_+-";
+var numbers = "1234567890";
+var alphabetlower = "abcdefghijklmnopqrstuvwxyz";
+var alphabetupper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var sliderlength = document.getElementById("sliderlength");
+var sliderlength = document.getElementById("numbox");
+var sliderlength = document.getElementById("symbox");
+var sliderlength = document.getElementById("lowerbox");
+var sliderlength = document.getElementById("upperbox");
+
+function generatepwd() {}
