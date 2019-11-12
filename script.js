0198af3cf106421e65eeb460ad0e41ebe2119acb
Philip Ng <phil__n@hotmail.com>
2019-11-11 23:45:11 -0500

    created function for generating a password based on a specified character length and tested with console log

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var symbols = "!@#$%^&*()_+-";
 var numbers = "1234567890";
-var alphabetlower = "abcdefghijklmnopqrstuvwxyz";
+var characters = "abcdefghijklmnopqrstuvwxyz";
 var alphabetupper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var sliderlength = document.getElementById("sliderlength");
-var sliderlength = document.getElementById("numbox");
-var sliderlength = document.getElementById("symbox");
-var sliderlength = document.getElementById("lowerbox");
-var sliderlength = document.getElementById("upperbox");
+var numbox = document.getElementById("numbox");
+var symbox = document.getElementById("symbox");
+var upperbox = document.getElementById("upperbox");
+var submit = document.getElementById("submit");
+var pwdbox = document.getElementById("displaypwd");
 
-function generatepwd() {}
+console.log(generatepwd(50, characters));
+
+function generatepwd(length, characters) {
+  var pwdgen = "";
+  for (var i = 0; i < length; i++) {
+    pwdgen += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return pwdgen;
+}
